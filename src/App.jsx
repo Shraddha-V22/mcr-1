d2b5facc62c3f80159911effb58654236788a839
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-17 19:01:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 underline shadow-md">
+      Tailwind setup
+    </div>
+  );
+}
+
+export default App;
